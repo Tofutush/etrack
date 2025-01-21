bda19f1ff28850435439c6c85ba533d5379512be
Tofutush <xiexuanqisirocco@163.com>
2025-01-21 19:27:09 +0800

    credit

---
+++ src/uicrap/credits.tsx
@@ -0,0 +1,13 @@
+function Credits() {
+    return (
+        <>
+            <p>Brought to you by:</p>
+            <p>tr1: hello world</p>
+            <p><a href="https://smsfoundation4.wordpress.com">（嫖资料来这里，催嘶为爱发电）</a></p>
+            <p>黑帮老大：不能 full stack，没有前途。</p>
+            <p><a href="https://tofutush.github.io/The-Iron-Ragdoll/">（老大私货）</a></p>
+        </>
+    )
+}
+
+export default Credits;

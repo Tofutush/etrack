579ab47c3240710ee0c813cd7d6cfba7470d9818
Tofutush <xiexuanqisirocco@163.com>
2025-01-28 15:55:44 +0800

    update msg

--- src/uicrap/Footer.tsx
+++ src/uicrap/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
             <p>Brought to you by:</p>
             <p>tr1: hello world</p>
             <p><a href="https://smsfoundation4.wordpress.com">（嫖资料来这里，催嘶为爱发电）</a></p>
-            <p>黑帮老大：不能 full stack，没有前途。</p>
+            <p>黑帮老大：我是！黑！帮！老！大！</p>
             <p><a href="https://tofutush.github.io/The-Iron-Ragdoll/">（老大私货）</a></p>
             <p>草台算法，不能保证准确性。</p>
             <p>如有出入，老大概不负责。</p>
